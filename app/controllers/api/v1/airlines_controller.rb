05bd72c35e18884d38d5024151c4b297c60711f5
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-04 16:28:19 +0900

    implemented api endpoints for airlines + reviews

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,22 +1,24 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
-      def index
-        airlines = Airlines.all
+      protect_from_forgery with: :null_session
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+      def index
+        airlines = Airline.all
+
+        render json: AirlineSerializer.new(airlines).serializable_hash.to_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline).serializable_hash.to_json
       end
 
       def create
         airline = Airline.new(airline_params)
         if airline.save
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline).serializable_hash.to_json
         else
           render json: { error: airline.errors.messages }, status: 422
         end
@@ -25,7 +27,7 @@ module Api
       def update
         airline = Airline.find_by(slug: params[:slug])
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline).serializable_hash.to_json
         else
           render json: { error: airline.errors.messages }, status: 422
         end
@@ -33,7 +35,7 @@ module Api
 
       def destroy
         airline = Airline.find_by(slug: params[:slug])
-        if airline.destroy(airline_params)
+        if airline.destroy
           head :no_content
         else
           render json: { error: airline.errors.messages }, status: 422
