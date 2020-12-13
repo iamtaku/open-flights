3c71d228e0c0951bb508d82515f5cad72fbaa940
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-13 18:26:52 +0900

    started axios get method

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/v1/airlines.json");
+    //get all airlines from api
+    // use the setairlines method to update our state
+  });
   return <div>This is the Airlines#index for our app</div>;
 };
 
