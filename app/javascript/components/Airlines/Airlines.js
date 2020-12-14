5c95817ce8d21f485da5a03cd14bc3fde4aa7cb7
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-14 20:30:28 +0900

    updated airlinegriditem filename

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import AirlineGridItem from "./airlineGridItem";
+import AirlineGridItem from "./gridItem";
 
 const Home = styled.div`
   text-align: center;
