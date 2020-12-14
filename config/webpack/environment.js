8465faa467db825d14f3c2196f3c55a23574145c
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-14 20:46:25 +0900

    changed webpacker environment file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,37 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const path = require("path");
+
+module.exports = (env) => {
+  let prod = env !== undefined && env.production === true;
+
+  return {
+    devtool: prod ? "source-map" : "eval-cheap-module-source-map",
+
+    context: path.resolve(__dirname, "../../app/javascript/packs"),
+    entry: {
+      application: "application.js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+
+    output: {
+      path: path.resolve(__dirname, "../../dist/"),
+      filename: prod ? "[name].bundle.js" : "[name].js",
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  };
+};
+
+module.exports = environment;
